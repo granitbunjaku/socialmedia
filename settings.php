6a02a73f2b511311ce93ecaaaf8fb0bc3459d40c
granitbunjaku <granitbunjaku14@gmail.com>
2022-12-30 18:52:48 +0100

    updated settings to be able to change bio

--- settings.php
+++ settings.php
@@ -1,6 +1,10 @@
 <?php
 include('includes/header.php');
 
+$user = $crud->read("users", ['id' => $_SESSION['user_id']]);
+
+extract($user[0]);
+
 $errors = [];
 
 $success = [];
@@ -9,9 +13,13 @@ if ($_POST) {
     if (isset($_POST['update'])) {
         if (strlen($_POST['fullname'] > 0)) {
             if (strlen($_POST['fullname']) > 5) {
-                if ($crud->update("users", ['fullname' => $_POST['fullname']], ['column' => 'id', 'value' => $_SESSION['user_id']])) {
-                    $_SESSION['fullname'] = $_POST['fullname'];
-                    $success[] = "You updated your username successfully";
+                if($_POST['fullname'] !== $fullname){
+                    if ($crud->update("users", ['fullname' => $_POST['fullname']], ['column' => 'id', 'value' => $_SESSION['user_id']])) {
+                        $_SESSION['fullname'] = $_POST['fullname'];
+                        $success[] = "You updated your username successfully";
+                    }
+                } else {
+                    $errors[] = "Your fullname shouldn't be the same as the recent!";
                 }
             } else {
                 $errors[] = "Fullname should be longer than 5 characters!";
@@ -19,9 +27,21 @@ if ($_POST) {
         }
 
         if (strlen($_POST['email'] > 0)) {
-            if ($crud->update("users", ['email' => $_POST['email']], ['column' => 'id', 'value' => $_SESSION['user_id']])) {
-                $success[] = "You updated your username successfully";
+            if($_POST['email'] !== $email){
+                if ($crud->update("users", ['email' => $_POST['email']], ['column' => 'id', 'value' => $_SESSION['user_id']])) {
+                    $success[] = "You updated your username successfully";
+                }
+            } else {
+                $errors[] = "Your email shouldn't be the same as the recent!";
             }
+        }
+
+        if ($_POST['bio'] !== $bio) {
+            if ($crud->update("users", ['bio' => $_POST['bio']], ['column' => 'id', 'value' => $_SESSION['user_id']])) {
+                $success[] = "You updated your bio successfully";
+            }
+        } else {
+            $errors[] = "Your bio shouldn't be the same as the recent!";
         }
     }
 
@@ -80,11 +100,15 @@ if ($_POST) {
 
         <div class="container" style="margin-top: 30px;">
             <label for="inputFullname" class="form-label">Full Name</label>
-            <input type="text" id="inputFullname" class="form-control" name="fullname">
+            <input type="text" id="inputFullname" class="form-control" name="fullname" value="<?=$fullname?>">
         </div>
         <div class="container" style="margin-top: 20px;">
             <label for="inputEmail" class="form-label">Email Address</label>
-            <input type="email" id="inputEmail" class="form-control" name="email">
+            <input type="email" id="inputEmail" class="form-control" name="email" value="<?=$email?>">
+        </div>
+        <div class="container" style="margin-top: 20px;">
+            <label for="inputBio" class="form-label">Bio</label>
+            <input type="text" id="inputBio" class="form-control" name="bio" value="<?=$bio?>">
         </div>
         <div class="container" style="margin-top: 20px;">
             <button type="submit" class="form-control btn btn-primary" name="update">Update</button>
